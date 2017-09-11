3f90d71d017d8a572f470ab5cb07aa0685890da5
Rex Tsou <akccakccwww@gmail.com>
2017-09-11 15:21:04 +0800

    Init

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: [
+          'html-loader',
+          'pug-html-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'sass-loader',
+          ],
+          publicPath: path.resolve(__dirname, 'dist')
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: './src/views/index.pug'
+    }),
+    new ExtractTextPlugin({
+      filename: 'main.css',
+      disable: false,
+      allChunks: true
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ]
+};
