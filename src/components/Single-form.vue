c57dd1d453bef4812bf108455d94add8ba92316b
Rex Tsou <akccakccwww@gmail.com>
2017-09-12 18:53:36 +0800

    Update Single-form.vue

--- src/components/Single-form.vue
+++ src/components/Single-form.vue
@@ -51,19 +51,68 @@
         select#receipt-town.select
           option(selected, disabled) 鄉鎮市區
         input#receipt-address.input(type="text", placeholder="請輸入地址")
-    label.label.label--radio(for="receipt-check")
-      span.text
-      input#receipt-check.input(type="checkbox", data-text="收據資訊同通訊資訊")
-
+    label.label.label--checkbox(for="receipt-check", :class="{ 'is-checked': receiptCheck }", @click.prevent="toggleCheckbox")
+      .radio
+        input#receipt-check.input(type="checkbox", data-vue="receiptCheck")
+      span.text 收據資訊同通訊資訊
+    .label-wrap
+      span.text 是否願意收到實體文宣，了解TFT的近況與成果？
+      .radio-container
+        label.label.label--radio(for="campaign-1", :class="{ 'is-checked': campaign[1] }")
+          .radio
+            input#campaign-1.input(name="campaign", type="radio", data-vue="1", value="y", @change="toggleRadio")
+          span.text 是
+        label.label.label--radio(for="campaign-2", :class="{ 'is-checked': campaign[2] }")
+          .radio
+            input#campaign-2.input(name="campaign", type="radio", data-vue="2", value="n", @change="toggleRadio")
+          span.text 否
+    .label-wrap
+      span.text 是否願意訂閱TFT電子報？
+      .radio-container
+        label.label.label--radio(for="newsletter-1", :class="{ 'is-checked': newsletter[1] }")
+          .radio
+            input#newsletter-1.input(name="newsletter", type="radio", data-vue="1", value="y", @change="toggleRadio")
+          span.text 是
+        label.label.label--radio(for="newsletter-2", :class="{ 'is-checked': newsletter[2] }")
+          .radio
+            input#newsletter-2.input(name="newsletter", type="radio", data-vue="2", value="n", @change="toggleRadio")
+          span.text 否
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      receiptCheck: false,
+      campaign: {
+        1: true,
+        2: false,
+      },
+      newsletter: {
+        1: true,
+        2: false,
+      }
+    };
+  },
+  methods: {
+    toggleCheckbox(e) {
+      const targetData = e.currentTarget.querySelector('.input[type="checkbox"]').dataset.vue;
+      this[targetData] = !this[targetData];
+    },
+    toggleRadio(e) {
+      const targetData = e.target.name;
+      const targetOrder = e.target.dataset.vue;
+      for (let key in this[targetData]) {
+        this[targetData][key] = false;
+      }
+      this[targetData][targetOrder] = true;
+    }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../sass/variable';
 .form {
   max-width: 500px;
   margin: 0 auto;
@@ -71,6 +120,15 @@ export default {
   flex-direction: column;
   flex-wrap: wrap;
   justify-content: center;
+  .label-wrap {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin: 0;
+    .label {
+      margin: 0;
+    }
+  }
 
   .label {
     display: flex;
@@ -78,7 +136,7 @@ export default {
     align-items: flex-start;
     margin: 10px 0;
     .text {
-      min-width: 40%;
+      min-width: 32%;
       text-align: right;
       padding: .55rem 1rem;
     }
@@ -96,19 +154,60 @@ export default {
         margin-bottom: 15px;
       }
     }
-    &--radio {
-      .input {
+    @mixin radio($color: $c-primary) {
+      .radio {
+        margin-right: 5px;
+        background: #e6e6e6;
+        width: 18px;
+        height: 18px;
         position: relative;
+        border: 1px solid #979797;
+        background: #FFF;
+        border-radius: 50%;
+        position: relative;
+        .input {
+          opacity: 0;
+        }
         &:after {
-          content: attr(data-text);
+          content: '';
           position: absolute;
-          right: 0;
-
+          left: 50%;
+          top: 50%;
+          width: 0;
+          height: 0;
+          border-radius: 50%;
+          background: $color;
+          transform: translate(-50%, -50%);
+          transition: .1s ease-in-out;
         }
       }
+      &.is-checked {
+        .radio:after {
+          width: 60%;
+          height: 60%;
+        }
+      }
+      .text {
+        min-width: initial;
+      }
+    }
+    &--checkbox {
+      align-items: center;
+      @include radio();
+    }
+    &--radio {
+      @include radio();
+    }
+  }
+  .radio-container {
+    display: flex;
+    .label {
+      align-items: center;
+      padding: .55em .5em;
+    }
+    .text {
+      padding: .4em;
     }
   }
 }
 </style>
-
-
