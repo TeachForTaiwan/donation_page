d39bb6f500f70a00fa5c2e69056bcd3bd8e6030c
Rex Tsou <akccakccwww@gmail.com>
2017-09-13 17:24:54 +0800

    Improve form data bindings

--- src/components/Single-form.vue
+++ src/components/Single-form.vue
@@ -2,29 +2,29 @@
   form#form.form
     label.label(for="name")
       span.text 姓名／單位名稱*
-      input#name.input(type="text", placeholder="請輸入姓名／單位名稱")
+      input#name.input(type="text", placeholder="請輸入姓名／單位名稱", v-model="formData.name")
     label.label(for="code", placeholder="請輸入企業代碼")
       span.text 企業代碼
-      input#code.input(type="text")
+      input#code.input(type="text", v-model="formData.code")
     label.label(for="id-number")
       span.text 身分證字號／統一編號
-      input#id-number.input(type="text", placeholder="請輸入身分證字號／統一編號")
+      input#id-number.input(type="text", placeholder="請輸入身分證字號／統一編號", v-model="formData.idNumber")
     label.label(for="gender")
       span.text 性別
-      select#gender.select
+      select#gender.select(v-model="formData.gender")
         option(selected, disabled) 請選擇性別
         option(value="0") 男
         option(value="1") 女
         option(value="2") 其他
     label.label(for="birth")
       span.text 出生日期
-      input#birth.input(type="date")
+      input#birth.input(type="date", v-model="formData.birth")
     label.label(for="tel")
       span.text 聯絡電話*
-      input#tel.input(type="tel", placeholder="請輸入聯絡電話")
+      input#tel.input(type="tel", placeholder="請輸入聯絡電話", v-model="formData.tel")
     label.label(for="email")
       span.text 電子信箱*
-      input#email.input(type="email", placeholder="請輸入電子信箱")
+      input#email.input(type="email", placeholder="請輸入電子信箱", v-model="formData.email")
     label.label
       span.text 通訊地址*
       .input-set
@@ -35,18 +35,18 @@
         input#address.input(type="text", placeholder="請輸入地址")
     label.label(for="receipt")
       span.text 收據寄發*
-      select#receipt.select
+      select#receipt.select(v-model="formData.receipt")
         option(selected, disabled) 請選擇是否需要收據
         option(value="0") 年度收據
         option(value="1") 單次收據
         option(value="2") 不寄收據
     label.label(for="receipt-title")
       span.text 收據抬頭*
-      input#receipt-title.input(type="text", placeholder="請輸入收據抬頭")
-    label.label.label--checkbox(for="receipt-check", :class="{ 'is-checked': receiptCheck }", @click.prevent="toggleCheckbox")
+      input#receipt-title.input(type="text", placeholder="請輸入收據抬頭", v-model="formData.receiptAddress")
+    label.label.label--checkbox(for="receipt-check", :class="{ 'is-checked': formData.receiptCheck }", @click.prevent="formData.receiptCheck = !formData.receiptCheck")
       .radio-container
         .radio
-          input#receipt-check.input(type="checkbox", data-vue="receiptCheck")
+          input#receipt-check.input(type="checkbox")
       span.text 收據資訊同通訊資訊
     label.label
       span.text 收據地址*
@@ -59,24 +59,24 @@
     .label-wrap
       span.text 是否願意收到實體文宣，了解TFT的近況與成果？
       .radio-container
-        label.label.label--radio(for="campaign-1", :class="{ 'is-checked': campaign[1] }")
+        label.label.label--radio(for="campaign-1", :class="{ 'is-checked': formData.campaign === 'true' }")
           .radio
-            input#campaign-1.input(name="campaign", type="radio", data-vue="1", value="y", @change="toggleRadio")
+            input#campaign-1.input(name="campaign", type="radio", value="true", v-model="formData.campaign")
           span.text 是
-        label.label.label--radio(for="campaign-2", :class="{ 'is-checked': campaign[2] }")
+        label.label.label--radio(for="campaign-2", :class="{ 'is-checked': formData.campaign === 'false' }")
           .radio
-            input#campaign-2.input(name="campaign", type="radio", data-vue="2", value="n", @change="toggleRadio")
+            input#campaign-2.input(name="campaign", type="radio", value="false", v-model="formData.campaign")
           span.text 否
     .label-wrap
       span.text 是否願意訂閱TFT電子報？
       .radio-container
-        label.label.label--radio(for="newsletter-1", :class="{ 'is-checked': newsletter[1] }")
+        label.label.label--radio(for="newsletter-1", :class="{ 'is-checked': formData.newsletter === 'true' }")
           .radio
-            input#newsletter-1.input(name="newsletter", type="radio", data-vue="1", value="y", @change="toggleRadio")
+            input#newsletter-1.input(name="newsletter", type="radio", value="true", v-model="formData.newsletter")
           span.text 是
-        label.label.label--radio(for="newsletter-2", :class="{ 'is-checked': newsletter[2] }")
+        label.label.label--radio(for="newsletter-2", :class="{ 'is-checked': formData.newsletter === 'false' }")
           .radio
-            input#newsletter-2.input(name="newsletter", type="radio", data-vue="2", value="n", @change="toggleRadio")
+            input#newsletter-2.input(name="newsletter", type="radio", value="false", v-model="formData.newsletter")
           span.text 否
     .btn-container
       router-link.btn.btn--grey(to="single") 回上一步
@@ -88,29 +88,25 @@ export default {
   data() {
     return {
       receiptCheck: false,
-      campaign: {
-        1: true,
-        2: false,
-      },
-      newsletter: {
-        1: true,
-        2: false,
+      formData: {
+        name: '',
+        code: '',
+        idNumber: '',
+        gender: '',
+        birth: '',
+        tel: '',
+        email: '',
+        address: '',
+        receipt: '',
+        receiptCheck: '',
+        receiptTitle: '',
+        receiptAddress: '',
+        campaign: '',
+        newsletter: '',
       },
     };
   },
   methods: {
-    toggleCheckbox(e) {
-      const targetData = e.currentTarget.querySelector('.input[type="checkbox"]').dataset.vue;
-      this[targetData] = !this[targetData];
-    },
-    toggleRadio(e) {
-      const targetData = e.target.name;
-      const targetOrder = e.target.dataset.vue;
-      for (let key in this[targetData]) {
-        this[targetData][key] = false;
-      }
-      this[targetData][targetOrder] = true;
-    },
   },
 };
 </script>
